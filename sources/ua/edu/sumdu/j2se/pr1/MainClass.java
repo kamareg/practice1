3b7a31562511dd668405b313019570af85207606
kamareg <kamareggg@gmail.com>
2022-05-11 15:12:47 +0300

    new comment just for learning github

--- sources/ua/edu/sumdu/j2se/pr1/MainClass.java
+++ sources/ua/edu/sumdu/j2se/pr1/MainClass.java
@@ -1,6 +1,7 @@
 package ua.edu.sumdu.j2se.pr1;
 import ua.edu.sumdu.j2se.pr1.operations.Operation;
 public class MainClass {
+    //change for git
 public static void main (String [] args) {
 for(int x = 1; x<10; x ++) {
 for(int y = 1; y <10; y ++) {
